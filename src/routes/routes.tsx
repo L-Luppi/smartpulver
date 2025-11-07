c19c5f3ddb650fd021ae5b27a20006b83328ed38
igor <igor.mtka@gmail.com>
2025-11-07 01:02:08 -0300

    coloca outras rotas

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -17,8 +17,11 @@ export const router = (
   createBrowserRouter([
     // 🔹 Callback do Cognito (pública)
 
-    { path: "/callback", element: <LoginCallback /> },
-
+    { path: "/login/*", element: <LoginCallback /> },
+    {
+      path: "/callback",
+      element: <LoginCallback />,
+    },
     {
       path: "/logged-out",
       element: <LoggedOut />,
