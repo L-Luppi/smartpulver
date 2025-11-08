79803ba2c1467e9eacd9912511accf076d439800
igor <igor.mtka@gmail.com>
2025-11-08 18:54:36 -0300

    corrige o logout

--- src/components/molecules/SidebarFooter.tsx
+++ src/components/molecules/SidebarFooter.tsx
@@ -1,29 +1,24 @@
 import { useState } from "react";
 import { Box, Divider, List } from "@mui/material";
+import { useAuth } from "react-oidc-context";
 import UserAvatarButton from "./UserAvatarButton";
 import UserDropdownMenu from "../molecules/UserDropdownMenu";
 
 export default function SidebarFooter() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+  const auth = useAuth();
 
   const handleOpen = (event: React.MouseEvent<HTMLElement>) =>
     setAnchorEl(event.currentTarget);
 
   const handleClose = () => setAnchorEl(null);
 
-const signOutRedirect = () => {
-  const clientId = import.meta.env.VITE_COGNITO_CLIENT_ID;
-  const cognitoDomain = import.meta.env.VITE_COGNITO_DOMAIN;
-  const postLogoutRedirect = import.meta.env.VITE_COGNITO_LOGOUT_REDIRECT_URI;
-
-  window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(
-    postLogoutRedirect
-  )}`;
-
-   localStorage.clear(); 
-  sessionStorage.clear();
-};
+  const handleLogout = () => {
+    auth.signoutRedirect({
+      post_logout_redirect_uri: import.meta.env.VITE_COGNITO_LOGOUT_REDIRECT_URI,
+    });
+  };
 
   return (
     <Box>
@@ -40,7 +35,7 @@ const signOutRedirect = () => {
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        onLogout={signOutRedirect}
+        onLogout={handleLogout}
       />
     </Box>
   );
