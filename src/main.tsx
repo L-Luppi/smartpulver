a2a9b85c7474e4fb43771cd4073ff037bddc8fee
igor <igor.mtka@gmail.com>
2025-11-08 18:35:09 -0300

    altera o authority dentro da main

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "@aws-amplify/ui-react/styles.css";
 import { AuthProvider } from "react-oidc-context";
 
 const cognitoAuthConfig = {
-  authority: import.meta.env.VITE_COGNITO_AUTHORITY,
+  authority: `https://${import.meta.env.VITE_COGNITO_DOMAIN}`,
   client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
   redirect_uri: import.meta.env.VITE_COGNITO_REDIRECT_URI,
   response_type: import.meta.env.VITE_COGNITO_RESPONSE_TYPE,
