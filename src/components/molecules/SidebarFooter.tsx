a346b35bbf66194bfd29f5c35c7cbdf98e65947a
igor <igor.mtka@gmail.com>
2025-11-09 20:37:15 -0300

    testa sign out exemplo do cognito

--- src/components/molecules/SidebarFooter.tsx
+++ src/components/molecules/SidebarFooter.tsx
@@ -2,12 +2,10 @@ import { useState } from "react";
 import { Box, Divider, List } from "@mui/material";
 import UserAvatarButton from "./UserAvatarButton";
 import UserDropdownMenu from "../molecules/UserDropdownMenu";
-import { useAuth } from "react-oidc-context";
 
 export default function SidebarFooter() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
-  const auth = useAuth();
 
   const handleOpen = (event: React.MouseEvent<HTMLElement>) =>
     setAnchorEl(event.currentTarget);
