bb42074b4488665a9e82ec423c89deb71b0e30ef
igor <igor.mtka@gmail.com>
2025-11-19 11:13:48 -0300

    corrige parse das coordenadas

--- src/utils/mapUpdater.ts
+++ src/utils/mapUpdater.ts
@@ -1,15 +1,25 @@
-import { useMap } from "react-leaflet";
+import { useMap, Map } from "react-leaflet";
 import { useEffect } from "react";
 
-export default function MapUpdater({ lat, lng }) {
-  const map = useMap();
+interface MapUpdaterProps {
+  lat: number;
+  lng: number;
+}
+
+export default function MapUpdater({ lat, lng }: MapUpdaterProps) {
+  const map = useMap(); // map é do tipo Map
 
   useEffect(() => {
+    if (!map) return;
+
     map.setView([lat, lng]);
 
-    setTimeout(() => {
+    // Garante que o Leaflet recalcule o tamanho depois do layout estar pronto
+    const timeoutId = setTimeout(() => {
       map.invalidateSize();
     }, 200);
+
+    return () => clearTimeout(timeoutId);
   }, [lat, lng, map]);
 
   return null;
